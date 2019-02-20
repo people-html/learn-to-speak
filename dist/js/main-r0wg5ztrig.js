407c79b88575c2799e1f6e88a06bb1b4b735e24d
PUGE <mail@puge.me>
2019-02-20 17:05:19 +0800

    start

---
+++ dist/js/main-r0wg5ztrig.js
@@ -0,0 +1,233 @@
+"use strict";
+
+// 对象合并方法
+function assign(a, b) {
+  var newObj = {};
+
+  for (var key in a) {
+    newObj[key] = a[key];
+  }
+
+  for (var key in b) {
+    newObj[key] = b[key];
+  }
+
+  return newObj;
+} // 运行页面所属的方法
+
+
+function runPageFunction(pageName, entryDom) {
+  // ozzx-name处理
+  window.ozzx.domList = {};
+  pgNameHandler(entryDom); // 判断页面是否有自己的方法
+
+  var newPageFunction = window.ozzx.script[pageName];
+  if (!newPageFunction) return; // console.log(newPageFunction)
+  // 如果有created方法则执行
+
+  if (newPageFunction.created) {
+    // 注入运行环境
+    newPageFunction.created.apply(assign(newPageFunction, {
+      $el: entryDom,
+      data: newPageFunction.data,
+      methods: newPageFunction.methods,
+      activePage: window.ozzx.activePage,
+      domList: window.ozzx.domList
+    }));
+  } // 判断页面是否有下属模板,如果有运行所有模板的初始化方法
+
+
+  for (var key in newPageFunction.template) {
+    var templateScript = newPageFunction.template[key];
+
+    if (templateScript.created) {
+      // 获取到当前配置页的DOM
+      // 待修复,临时获取方式,这种方式获取到的dom不准确
+      var domList = entryDom.getElementsByClassName('ox-' + key);
+
+      if (domList.length !== 1) {
+        console.error('我就说会有问题吧!');
+        console.log(domList);
+      } // 为模板注入运行环境
+
+
+      templateScript.created.apply(assign(newPageFunction.template[key], {
+        $el: domList[0].children[0],
+        data: templateScript.data,
+        methods: templateScript.methods,
+        activePage: window.ozzx.activePage,
+        domList: window.ozzx.domList
+      }));
+    }
+  }
+} // ozzx-name处理
+
+
+function pgNameHandler(dom) {
+  // 遍历每一个DOM节点
+  for (var i = 0; i < dom.children.length; i++) {
+    var tempDom = dom.children[i]; // 判断是否存在@name属性
+
+    var pgName = tempDom.attributes['@name'];
+
+    if (pgName) {
+      // console.log(pgName.textContent)
+      // 隐藏元素
+      tempDom.hide = function () {
+        this.style.display = 'none';
+      };
+
+      window.ozzx.domList[pgName.textContent] = tempDom;
+    } // 判断是否有点击事件
+
+
+    var clickFunc = tempDom.attributes['@click'];
+
+    if (clickFunc) {
+      tempDom.onclick = function () {
+        var clickFor = this.attributes['@click'].textContent; // 判断页面是否有自己的方法
+
+        var newPageFunction = window.ozzx.script[window.ozzx.activePage]; // console.log(this.attributes)
+        // 判断是否为模板
+
+        var templateName = this.attributes['template']; // console.log(templateName)
+
+        if (templateName) {
+          newPageFunction = newPageFunction.template[templateName.textContent];
+        } // console.log(newPageFunction)
+        // 取出参数
+
+
+        var parameterArr = [];
+        var parameterList = clickFor.match(/[^\(\)]+(?=\))/g);
+
+        if (parameterList && parameterList.length > 0) {
+          // 参数列表
+          parameterArr = parameterList[0].split(','); // 进一步处理参数
+
+          for (var i = 0; i < parameterArr.length; i++) {
+            var parameterValue = parameterArr[i].replace(/(^\s*)|(\s*$)/g, ""); // console.log(parameterValue)
+            // 判断参数是否为一个字符串
+
+            if (parameterValue.charAt(0) === '"' && parameterValue.charAt(parameterValue.length - 1) === '"') {
+              parameterArr[i] = parameterValue.substring(1, parameterValue.length - 2);
+            }
+
+            if (parameterValue.charAt(0) === "'" && parameterValue.charAt(parameterValue.length - 1) === "'") {
+              parameterArr[i] = parameterValue.substring(1, parameterValue.length - 2);
+            } // console.log(parameterArr[i])
+
+          }
+
+          clickFor = clickFor.replace('(' + parameterList + ')', '');
+        } // console.log(newPageFunction)
+        // 如果有方法,则运行它
+
+
+        if (newPageFunction.methods[clickFor]) {
+          // 绑定window.ozzx对象
+          // console.log(tempDom)
+          newPageFunction.methods[clickFor].apply({
+            $el: this,
+            activePage: window.ozzx.activePage,
+            domList: window.ozzx.domList,
+            data: newPageFunction.data,
+            methods: newPageFunction.methods
+          }, parameterArr);
+        }
+      };
+    } // 递归处理所有子Dom结点
+
+
+    if (tempDom.children.length > 0) {
+      pgNameHandler(tempDom);
+    }
+  }
+} // 页面资源加载完毕事件
+
+
+window.onload = function () {
+  var page = globalConfig.entry;
+  window.ozzx.activePage = page;
+  var entryDom = document.getElementById('ox-' + page);
+
+  if (entryDom) {
+    runPageFunction(page, entryDom);
+  } else {
+    console.error('找不到页面入口!');
+  }
+};
+
+window.ozzx = {
+  script: {},
+  tool: {}
+};
+var globalConfig = {
+  "root": "/src",
+  "entry": "main",
+  "title": "页面",
+  "outFolder": "./dist",
+  "watcher": {
+    "enable": true,
+    "folder": "./src",
+    "ignored": "./dist/*",
+    "depth": 99
+  },
+  "outPut": {
+    "minifyCss": false,
+    "minifyJs": false,
+    "choiceAnimation": false,
+    "globalStyle": "./src/main.css",
+    "outFileAddVersion": true
+  },
+  "serverPort": 8000,
+  "server": true,
+  "autoReload": true,
+  "headList": [{
+    "http-equiv": "content-type",
+    "content": "text/html; charset=UTF-8"
+  }, {
+    "name": "viewport",
+    "content": "initial-scale=1,user-scalable=no,maximum-scale=1"
+  }],
+  "scriptList": [],
+  "pageList": [{
+    "main": true,
+    "isPage": true,
+    "name": "main",
+    "src": "./src/page/main.page",
+    "temple": "<temple name=\"main\" src=\"./src/page/main.page\" isPage=\"true\"></temple>"
+  }],
+  "isOnePage": true
+};
+window.ozzx.script = {
+  "main": {
+    "data": {
+      "nameList": {
+        "rank1": {
+          "name": "lis",
+          "like": "orange"
+        },
+        "rank2": {
+          "name": "kim",
+          "like": "yellow"
+        },
+        "rank3": {
+          "name": "tony",
+          "like": "white"
+        }
+      }
+    },
+    "created": function created() {
+      console.log('hellow word!');
+    },
+    "methods": {
+      "showAlert": function showAlert(othersName, myName) {
+        console.log('可以传递参数:', othersName, myName);
+        console.log('方便的获取到可能会用到的信息!');
+        console.log(this);
+        this.$el.innerText = "Hellow ".concat(othersName, ", My name is ").concat(myName);
+      }
+    }
+  }
+};
--- ozzx.js
+++ ozzx.js
@@ -1,45 +1,63 @@
-module.exports ={
+module.exports = {
   // 项目根目录
-  "root": "/src",
+  root: "/src",
   // 项目入口文件
-  "entry": "voice",
-  "headFolder": "head",
+  entry: "main",
+  // 页面标题
+  title: '页面',
   // 输出目录
-  "outFolder": "dist",
+  outFolder: "./dist",
   // 是否监测文件改动重新打包
-  "autoPack": true,
-  // 是否压缩css
-  "minifyCss": false,
-  // 是否压缩js
-  "minifyJs": false,
-  // 页面目录
-  "pageFolder": "page",
-  // 强制打包所有样式
-  "choiceAnimation": false,
-  // 引用连接
-  "scriptList": [{
-    "name": "gif",
-    "src": "./src/script/gif.js",
-    "babel": true
-  },{
-    "name": "Animate",
-    "src": "./src/script/Animate.js",
-    "babel": false
-  },{
-    "name": "color",
-    "src": "./src/script/color.js",
-    "babel": false
-  },{
-    "name": "pixi-transform-tool",
-    "src": "./src/script/pixi-transform-tool.js",
-    "babel": false
-  },{
-    "name": "pixi.min",
-    "src": "./src/script/pixi.min.js",
-    "babel": false
-  },{
-    "name": "Scroller",
-    "src": "./src/script/Scroller.js",
-    "babel": false
-  }] 
+  watcher: {
+    // 是否启用
+    enable: true,
+    // 监控的文件夹 默认'./src'
+    folder: './src',
+    // 忽略监控的文件或文件夹，支持正则，默认为输出目录
+    ignored: './dist/*',
+    // 监测深度,默认99
+    depth: 99
+  },
+  // 输出配置
+  outPut: {
+    // 是否压缩css
+    minifyCss: false,
+    // 是否压缩js
+    minifyJs: false,
+    // 强制打包所有样式
+    choiceAnimation: false,
+    // 全局样式文件
+    globalStyle: './src/main.css',
+    // 输出文件自动追加版本号，可以用来消除缓存
+    outFileAddVersion: true,
+  },
+  serverPort: 8000,
+  // 静态文件服务
+  server: true,
+  // 自动重新加载
+  autoReload: true,
+  // head属性清单
+  headList: [
+    {
+      'http-equiv': 'content-type',
+      content: 'text/html; charset=UTF-8',
+    },
+    {
+      name: 'viewport',
+      content: 'initial-scale=1,user-scalable=no,maximum-scale=1',
+    }
+  ],
+  // 使用到的外部脚本清单
+  scriptList: [
+  ],
+  // 页面清单
+  pageList: [
+    {
+      // 是否为页面主入口
+      main: true,
+      isPage: true,
+      name: 'main',
+      src: './src/page/main.page'
+    }
+  ]
 }
